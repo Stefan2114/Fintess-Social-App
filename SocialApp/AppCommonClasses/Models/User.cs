de678314d8a82b91c06833a58ea4598bf026616d
Stefan2114 <stefan.toma021@gmail.com>
2025-05-26 01:05:18 -0700

    changed to int and added comments

--- SocialApp/AppCommonClasses/Models/User.cs
+++ SocialApp/AppCommonClasses/Models/User.cs
@@ -36,14 +36,26 @@ namespace AppCommonClasses.Models
         [Column("goal")]
         public string Goal { get; set; }
 
-        [Column("total_protein")]
-        public double? TotalProtein { get; set; }
 
-        [Column("total_carbohydrates")]
-        public double? TotalCarbohydrates { get; set; }
+        //we need to calculate the total macros for a person by this formula:
+        // basal_metabolism_rate = 10 * weight + 5 * height
+        // if female: basal_metabolism_rate -= 200
+        // total_daily_energy_expenditure = basal_metabolism_rate * activity_level_multyplier (1.2, 1.4, 1.6, 1,7, 1.9)
+        // if goal == "lose weight": calories = total_daily_energy_expenditure - 500
+        // if goal == "gain muscles": calories = total_daily_energy_expenditure + 500
+        // if goal == "maintain": calories = total_daily_energy_expenditure
+        // protein = 30%
+        // carbohydrates = 40%
+        // fat = 30%
 
-        [Column("total_fat")]
-        public double? TotalFat { get; set; }
+        [Column("proteins")]
+        public int Proteins { get; set; }
+
+        [Column("carbohydrates")]
+        public int Carbohydrates { get; set; }
+
+        [Column("fats")]
+        public int Fats { get; set; }
 
     }
 }
